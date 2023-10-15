67acc0d1bcd1480a7da39413477afe84b957a5b7
Natalia <115610226+NataliaN24@users.noreply.github.com>
2023-10-15 21:44:37 +0300

    Update 6.cpp

--- Exercises for test1 /6.cpp
+++ Exercises for test1 /6.cpp
@@ -67,3 +67,48 @@ int main() {
 
     return 0;
 }
+
+//Another solution
+int main() {
+    int n, m;
+    cin >> n;
+
+    set<int> uniqueNumbers;  // A std::set is a container that stores unique elements in a sorted order.
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        uniqueNumbers.insert(num); 
+    }
+
+    cin >> m;
+
+    int sum = 0;
+
+    for (int i = 0; i < m; i++) {
+        int num;
+        cin >> num;
+
+        
+        if (uniqueNumbers.find(num) != uniqueNumbers.end()) {
+            sum += num;
+            uniqueNumbers.erase(num); //clear the num to not encounter any more time
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
+
+//uniqueNumbers.find(num): This part of the expression 
+//calls the find method on the uniqueNumbers set. The
+//find method is used to search for a specific value within 
+//the set. If the value num is found in the set, it returns 
+//an iterator (a pointer-like object) that points to the location 
+//of num within the set. If num is not found, it returns the
+//uniqueNumbers.end(), which represents the "end" of the set.
+
+//uniqueNumbers.end(): This represents the "end" iterator of 
+//the set. It does not point to any element within the set and
+//is used to signify the end of the set.
