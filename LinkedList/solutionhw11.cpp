20d0127bf9af1952bedb0014de1ec3b2c9339a87
Natalia <115610226+NataliaN24@users.noreply.github.com>
2023-10-28 23:21:11 +0300

    Create solutionhw11.cpp

---
+++ LinkedList/solutionhw11.cpp
@@ -0,0 +1,26 @@
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* list) {
+    
+   SinglyLinkedListNode*current=list;
+    
+    while(current!=nullptr){
+        
+        SinglyLinkedListNode*iterator=current;
+        
+        while(iterator->next!=nullptr){
+            
+            if(current->data != iterator->next->data){
+                iterator=iterator->next;
+            }
+            else{
+                iterator->next=iterator->next->next;
+                
+            }
+            
+        }
+         current=current->next;
+    }
+    
+    return list;
+
+     
+}
