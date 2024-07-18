ff9483f6f4af07d0c364cc2f2cc3dedcb7e6709d
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-07-18 19:04:01 +0300

    Update and rename 1.cpp to Stack.hpp

--- 05.Stack/Stack/Stack.hpp
+++ 05.Stack/Stack/Stack.hpp
@@ -173,26 +173,3 @@ size_t Stack<T>::size() const
 {
 	return topIndex;
 }
-
-int main() {
-	try {
-		Stack<int> myStack;
-
-		myStack.push(10);
-		myStack.push(20);
-		myStack.push(30);
-
-		std::cout << "Stack size: " << myStack.size() << std::endl;
-		std::cout << "Top element: " << myStack.peek() << std::endl;
-
-		myStack.pop();
-
-		std::cout << "Stack size after pop: " << myStack.size() << std::endl;
-		std::cout << "Top element after pop: " << myStack.peek() << std::endl;
-	}
-	catch (const std::exception& e) {
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-
-	return 0;
-}
