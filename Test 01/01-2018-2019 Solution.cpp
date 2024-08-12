f2365d551d08e078bfbbe873cf1bc7493ec7b096
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-08-12 18:13:42 +0300

    Update 01-2018-2019 Solution.cpp

--- Test 01/01-2018-2019 Solution.cpp
+++ Test 01/01-2018-2019 Solution.cpp
@@ -1,8 +1,7 @@
-
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+---------------------------------------------------FIRST SOLUTION---------------------------------------------------------------------------------------------------
 int main() {
     int n;
     cin >> n;
@@ -46,4 +45,64 @@ int main() {
     delete[] arr;
 
     return 0;
+}
+---------------------------------------------------SECOND SOLUTION---------------------------------------------------------------------------------------------------
+
+
+bool binarySearch(vector<int>&arr,int left,int right,int x)
+{
+    while(left<=right)
+    {
+        int mid=left+(right-left)/2;
+        
+        if(arr[mid]==x)
+        {
+            return true;
+        }
+        else if(arr[mid]<x)
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right=mid-1;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    std::sort(arr.begin(),arr.end());
+    
+    bool found=false;
+    int len=arr.size();
+    
+    for(int i=0;i<len-1;++i)
+    {
+        for(int j=i+1;j<len;j++)
+        {
+            int sum=arr[i]+arr[j];
+            if(binarySearch(arr,0,len-1,sum)){
+             found=true;
+                break;
+            }
+        }
+    }
+
+    if(found)
+    {
+        std::cout<<"true";
+    }
+    else
+    {
+    std::cout<<"false";
+    }
 }
