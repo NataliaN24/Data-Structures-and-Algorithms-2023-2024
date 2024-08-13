1bd211f1ae2a82338cd440e623323c8c9e6eb44b
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-08-13 15:52:38 +0300

    Update 09.cpp

--- Test 01/09.cpp
+++ Test 01/09.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 
-Given an array of integers numsand an integer target, return indices of the two numbers such that they add up to target.
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 //
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //
