466dfcb54256df3aee6d08eb8a9d238606872dc4
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-08-22 21:59:58 +0300

    Update 18 Exercise.cpp

--- LeetCode/18 Exercise.cpp
+++ LeetCode/18 Exercise.cpp
@@ -32,3 +32,55 @@ public:
         return find(node->left,k,seen)||find(node->right,k,seen);
     }
 };
+--------------------------------------------ANOTHER SOLUTION------------------------------------------------------------------------------------------------------
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(root==nullptr)
+        {
+            return false;
+        }
+        vector<int>arr;
+     inOrderTraversal(root, arr);
+        sort(arr.begin(),arr.end());
+        
+         int left = 0;
+        int right = arr.size() - 1;
+        
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return false;
+
+    }
+    private:
+     void inOrderTraversal(TreeNode* node, std::vector<int>& arr) {
+        if (node == nullptr) {
+            return;
+        }
+        inOrderTraversal(node->left, arr);
+        arr.push_back(node->val);  
+        inOrderTraversal(node->right, arr);
+    }
+
+   
+};
