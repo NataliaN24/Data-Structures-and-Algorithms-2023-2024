d9ac2c40c934d588e2ef33d84c7664cfbe502a68
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-08-25 20:23:00 +0300

    Update 49 Exercise.cpp

--- LeetCode/49 Exercise.cpp
+++ LeetCode/49 Exercise.cpp
@@ -41,3 +41,40 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+-------------------------------------------------------ANOTHER SOLUTION--------------------------------------------------------------------------------------------
+    class MyStack {
+
+   deque<int>dq;
+
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+       dq.push_back(x);
+    }
+    
+    int pop() {
+        int topEl=dq.back();
+        dq.pop_back();
+        return topEl;
+    }
+    
+    int top() {
+        return dq.back();
+    }
+    
+    bool empty() {
+        return dq.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
