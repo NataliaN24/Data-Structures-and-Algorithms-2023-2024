aedbc03ecb0c5ea3ba733835fc507c35a97ac2a5
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-08-26 19:29:36 +0300

    Update 08 Exercise.cpp

--- LeetCode/08 Exercise.cpp
+++ LeetCode/08 Exercise.cpp
@@ -50,3 +50,42 @@ public:
     }
 
 };
+---------------------------------------------ANOTHER SOLUTION------------------------------------------------------------------------------------------------------
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        if (head == nullptr) {
+            return head;
+        }
+
+        ListNode* left = head;
+        ListNode* right = head;
+        ListNode* current = head;
+
+        for (int i = 0; i < k - 1; i++) {
+            left = left->next;
+        }
+
+        current = left;
+        while (current->next) {
+            current = current->next;
+            right = right->next;
+        }
+
+        int temp = left->val;
+        left->val = right->val;
+        right->val = temp;
+
+        return head;
+    }
+};
