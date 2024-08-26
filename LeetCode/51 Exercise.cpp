e8c4ed28503df4c33b8b97a715ade097672c9375
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2024-08-26 12:23:01 +0300

    Create 51 Exercise.cpp

---
+++ LeetCode/51 Exercise.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>result;
+        for(const auto &i:intervals){
+            if(result.empty() || result.back()[1]<i[0])
+            {
+                result.push_back(i);
+            }
+            else{
+                result.back()[1]=max(result.back()[1],i[1]);
+
+            }
+        }
+        return result;
+    }
+};
